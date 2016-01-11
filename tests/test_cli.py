a7907eb2a385e6f111def723506c9353e9f2acd3
Robert Grant <rhgrant10@gmail.com>
2016-01-11 14:30:37 -0500

    Fix import mock differences among envs

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,7 +4,11 @@ try:
     import unittest2 as unittest
 except ImportError:
     import unittest
-from unittest.mock import patch
+
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
 
 from infoblox import cli
 
